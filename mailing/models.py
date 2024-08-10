0b7b6383c2a4e40a00a592b15a37c931e27326c1
vitek <rgavih@yandex.ru>
2024-08-10 19:56:13 +0700

    add fileds

---
+++ mailing/models.py
@@ -0,0 +1,80 @@
+from django.db import models
+
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Client(models.Model):
+    objects = None
+    email = models.EmailField()
+    full_name = models.CharField(max_length=100, verbose_name='ФИО', help_text='Введите ФИО')
+    comment = models.TextField(**NULLABLE)
+
+    class Meta:
+        verbose_name = 'Клиент'
+        verbose_name_plural = 'Клиенты'
+
+        def __str__(self):
+            return self.email
+
+
+class Message(models.Model):
+    objects = None
+    subject = models.CharField(max_length=255)
+    body = models.TextField()
+
+    class Meta:
+        verbose_name = 'Сообщение'
+        verbose_name_plural = 'Сообщения'
+
+        def __str__(self):
+            return self.subject
+
+
+class Mailing(models.Model):
+    DAILY = 'D'
+    WEEKLY = 'W'
+    MONTHLY = 'M'
+    PERIODICITY_CHOICES = [
+        (DAILY, 'Eжедневн'),
+        (WEEKLY, 'Еженедельно'),
+        (MONTHLY, 'Ежемесячно'),
+    ]
+
+    CREATED = 'CREATED'
+    STARTED = 'STARTED'
+    COMPLETED = 'COMPLETED'
+    STATUS_CHOICES = [
+        (CREATED, 'Created'),
+        (STARTED, 'Started'),
+        (COMPLETED, 'Completed'),
+    ]
+
+    start_datetime = models.DateTimeField()
+    periodicity = models.CharField(max_length=1, choices=PERIODICITY_CHOICES)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default=CREATED)
+    message = models.ForeignKey(Message, on_delete=models.CASCADE)
+    clients = models.ManyToManyField(Client)
+
+    class Meta:
+        verbose_name = 'Рассылка'
+        verbose_name_plural = 'Рассылки'
+
+        def __str__(self):
+            return f"{self.message.subject} - {self.start_datetime}"
+
+
+class MailingAttempt(models.Model):
+    mailing = models.ForeignKey(Mailing, on_delete=models.CASCADE, related_name='attempts')
+    attempt_datetime = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=20, choices=[
+        ('success', 'Success'),
+        ('failed', 'Failed')
+    ])
+    server_response = models.TextField(**NULLABLE)
+
+    class Meta:
+        verbose_name = 'Попытка'
+        verbose_name_plural = 'Попытки'
+
+        def __str__(self):
+            return f"{self.mailing} - {self.attempt_datetime}"
---
+++ mailing/views.py
@@ -0,0 +1,93 @@
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from .models import Client, Message, Mailing, MailingAttempt
+from django.shortcuts import render
+
+
+def home(request):
+    return render(request, 'base.html')
+
+
+class ClientListView(ListView):
+    model = Client
+    template_name = 'clients/client_list.html'
+    context_object_name = 'clients'
+
+
+class ClientCreateView(CreateView):
+    model = Client
+    template_name = 'clients/client_form.html'
+    success_url = reverse_lazy('mailing:client-list')
+    fields = ['email', 'full_name', 'comment']
+
+
+class ClientUpdateView(UpdateView):
+    model = Client
+    template_name = 'clients/client_form.html'
+    success_url = reverse_lazy('mailing:client-list')
+    fields = ['email', 'full_name', 'comment']
+
+
+class ClientDeleteView(DeleteView):
+    model = Client
+    template_name = 'clients/client_confirm_delete.html'
+    success_url = reverse_lazy('mailing:client-list')
+
+
+class MessageListView(ListView):
+    model = Message
+    template_name = 'message/message_list.html'
+    context_object_name = 'messages'
+
+
+class MessageCreateView(CreateView):
+    model = Message
+    template_name = 'message/message_form.html'
+    success_url = reverse_lazy('mailing:message-list')
+    fields = ['subject', 'body']
+
+
+class MessageUpdateView(UpdateView):
+    model = Message
+    template_name = 'message/message_form.html'
+    success_url = reverse_lazy('mailing:message-list')
+    fields = ['subject', 'body']
+
+
+class MessageDeleteView(DeleteView):
+    model = Message
+    template_name = 'message/message_confirm_delete.html'
+    success_url = reverse_lazy('mailing:message-list')
+    fields = ['subject', 'body']
+
+
+class MailingListView(ListView):
+    model = Mailing
+    template_name = 'mailings/mailing_list.html'
+    context_object_name = 'mailings'
+
+
+class MailingCreateView(CreateView):
+    model = Mailing
+    template_name = 'mailings/mailing_form.html'
+    success_url = reverse_lazy('mailing:mailing-list')
+    fields = ['start_datetime', 'periodicity', 'message', 'clients']
+
+
+class MailingUpdateView(UpdateView):
+    model = Mailing
+    template_name = 'mailings/mailing_form.html'
+    success_url = reverse_lazy('mailing:mailing-list')
+    fields = ['start_datetime', 'periodicity', 'message', 'clients']
+
+
+class MailingDeleteView(DeleteView):
+    model = Mailing
+    template_name = 'mailings/mailing_confirm_delete.html'
+    success_url = reverse_lazy('mailing:mailing-list')
+
+
+class AttemptListView(ListView):
+    model = MailingAttempt
+    template_name = 'attempt/attempt_list.html'
+    context_object_name = 'attempts'
